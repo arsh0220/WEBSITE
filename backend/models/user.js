518c9f603d57540a0a8ffd35b5d315b992b514ac
Arshdeep Kaur <137782831+arsh0220@users.noreply.github.com>
2025-02-06 15:58:32 +0530

    Delete backend directory

--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-    firstName:"string",
-    lastName:"string",
-    email:"string",
-    password:"string"
-},{timestamps:true});
-const User = mongoose.model("User", userSchema);
-export default User;
